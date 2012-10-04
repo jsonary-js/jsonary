d2e15bad07e6bc54461970d6f982acad84c2dfbb
user@server <geraintluff@gmail.com>
2012-10-04 09:12:36 +0100

    Updated jsonary.js, jsonary.hash.js

--- plugins/jsonary.hash.js
+++ plugins/jsonary.hash.js
@@ -1,2 +1,27 @@
 (function (global) {
+	var hashJsonaryData = Jsonary.create(null);
+	
+	var lastHash = null;
+	setInterval(function () {
+		var hashString = window.location.hash;
+		if (hashString.length > 0 && hashString.charAt(0) == "#") {
+			hashString = hashString.substring(1);
+		}
+		if (hashString == lastHash) {
+			return;
+		}
+		lastHash = hashString;
+		
+		var hashData = hashString;
+		try {
+			hashData = Jsonary.decodeData(hashString, "application/x-www-form-urlencoded");
+		} catch (e) {
+			console.log(e);
+		}
+	}, 100);
+
+	Jsonary.extend({
+		hash: hashJsonaryData
+	});	
+	
 })(this);
