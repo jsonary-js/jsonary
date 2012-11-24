72fb4e11c61a26bafb07cc7372752ffd0f1b3371
user@server <geraintluff@gmail.com>
2012-11-24 15:26:16 +0000

    Added exclusiveMinimum/exclusiveMaximum support

--- tests/tests/02 - Secondary tests/06 - Advanced schema matching - version 3.js
+++ tests/tests/02 - Secondary tests/06 - Advanced schema matching - version 3.js
@@ -249,3 +249,41 @@ tests.add("Min/max (number)", function () {
 	
 	return true;
 });
+
+
+tests.add("Exclusive min/max (number)", function () {
+	var data = Jsonary.create(5);
+	var schema = Jsonary.createSchema({
+		"minimum": 3,
+		"exclusiveMinimum": true,
+		"maximum": 10,
+		"exclusiveMaximum": true
+	});
+	
+	var match = null;
+	var failReason = null;
+	var notificationCount = 0;
+	var schemaKey = Jsonary.getMonitorKey();
+	data.addSchemaMatchMonitor(schemaKey, schema, function (m, fr) {
+		notificationCount++;
+		match = m;
+		failReason = fr;
+	});
+	
+	this.assert(match, "should match initially");
+	this.assert(notificationCount == 1, "notificationCount == 1, not " + notificationCount);
+
+	data.setValue(10);
+	this.assert(!match, "should not match after stage 2");
+	this.assert(notificationCount == 2, "notificationCount == 2, not " + notificationCount);
+
+	data.setValue(3);
+	this.assert(!match, "should not match after stage 3");
+	this.assert(notificationCount == 3, "notificationCount == 3, not " + notificationCount);
+
+	data.setValue(7);
+	this.assert(match, "should match after stage 4");
+	this.assert(notificationCount == 4, "notificationCount == 4, not " + notificationCount);
+	
+	return true;
+});
