69858d2feea6808250aa96059504018cfae5bb01
user@server <geraintluff@gmail.com>
2013-01-13 18:35:12 +0000

    Extra test

--- tests/tests/Bugs/Recursive dependencies.js
+++ tests/tests/Bugs/Recursive dependencies.js
@@ -24,3 +24,32 @@ tests.add("Recursive dependency check", function () {
 	
 	return true;
 });
+
+tests.add("Recursive dependency check 2", function () {
+	var data = Jsonary.create({
+	   "properties":{
+	      "createSchema":{
+		  "arguments":true
+	      }
+	   }
+	}, null, true);
+	var schema = Jsonary.createSchema({
+		"title": "API documentation",
+		"properties": {
+			"properties": {
+				"title": "Object properties",
+				"additionalProperties": {"$ref": "#"}
+			}
+		},
+		"dependencies": {
+			"arguments": {"title": "Dependency schema"}
+		}
+	});
+	data.addSchema(schema);
+	
+	this.assert(data.schemas().length == 1, "data.schemas().length == 1, was " + data.schemas().length);
+	var innerSchemas = data.property("properties").property("createSchema").schemas();
+	this.assert(innerSchemas.length == 2, "innerSchemas.length == 2, was " + innerSchemas.length);
+	
+	return true;
+});
