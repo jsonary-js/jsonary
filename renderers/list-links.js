24795816f609c165b2c0ed06a076c038f617bf9c
Geraint Luff <geraintluff@gmail.com>
2013-03-21 20:32:27 +0000

    Altered "list-links" renderer to do nothing for editable data

--- renderers/list-links.js
+++ renderers/list-links.js
@@ -9,6 +9,9 @@
 			return false;
 		},
 		renderHtml: function (data, context) {
+			if (data.readOnly()) {
+				return context.renderHtml(data);
+			}
 			var result = "";
 			
 			var links = data.links();
