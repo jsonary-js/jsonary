3dd1736ca5500b7720dc0b22709e4537d20d08ca
Geraint Luff <luffgd@gmail.com>
2013-04-24 08:54:26 +0100

    Allow modifying of pages

--- site.php
+++ site.php
@@ -7,7 +7,16 @@ include INCLUDE_ROOT.'json-utils.php';
 
 $pathInfo = $_SERVER['PATH_INFO'];
 $pathInfo = str_replace(".", "_", $pathInfo);
+$method = $_SERVER['REQUEST_METHOD'];
 $filename = "site{$pathInfo}.json";
+
+if ($method == "PUT" && $_SESSION[ADMINISTRATOR_FLAG]) {
+	$newData = json_decode(file_get_contents("php://input"));
+	if ($newData != NULL) {
+		file_put_contents($filename, json_encode($newData));
+	}
+}
+
 $jsonData = json_decode(file_get_contents($filename));
 
 if (substr($filename, 0, strlen("site/pages/")) == "site/pages") {
