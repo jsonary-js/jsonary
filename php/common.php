d141afa11e29c676fd4105e070ba4441d0512a58
Geraint Luff <luffgd@gmail.com>
2013-04-25 04:34:34 +0100

    Change back to true value

--- php/common.php
+++ php/common.php
@@ -1,6 +1,6 @@
 <?php
 
-define('SITE_ROOT', '/~geraint/jsonary/');
+define('SITE_ROOT', '/new/');
 define('INCLUDE_ROOT', dirname(__FILE__)."/");
 
 define('ADMINISTRATOR_FLAG', 'demo_blog_administrator');
