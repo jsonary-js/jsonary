e0afcf958c9ee28e9718ec68fb01641978ff6fb0
Ognian Tschakalov <ognian.tschakalov@ogi-it.com>
2013-07-15 17:06:45 +0200

    - fixed jsonary.render.table.js

--- plugins/jsonary.render.table.js
+++ plugins/jsonary.render.table.js
@@ -331,7 +331,7 @@
 					var valueA = a.get(path);
 					var valueB = b.get(path);
 					//var comparison = thisConfig.sort[path] ? thisConfig.sort[path](valueA, valueB) : thisConfig.defaultSort(valueA, valueB);
-					var comparison = thisConfig.sort[path] ? thisConfig.defaultSort(valueA, valueB) : thisConfig.sort[path](valueA, valueB);
+                    var comparison = (typeof thisConfig.sort[path] == 'function') ? thisConfig.sort[path](valueA, valueB) : thisConfig.defaultSort(valueA, valueB);
 					return multiplier*comparison;
 				});
 			}
