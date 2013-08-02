e8313742db5536628d138aac620633041c83016f
Geraint Luff <luffgd@gmail.com>
2013-08-02 18:46:47 +0100

    Style updates

--- renderers/plain.jsonary.js
+++ renderers/plain.jsonary.js
@@ -111,11 +111,10 @@
 			var result = "";
 			var basicTypes = data.schemas().basicTypes();
 			var enums = data.schemas().enumValues();
-			result += '<span class="dialog-anchor">';
 			if (context.uiState.dialogOpen) {
 				result += context.actionHtml('<div class="dialog-overlay"></div>', "closeDialog");
 				result += '<div class="dialog-box">';
-				result += context.actionHtml('<div class="dialog-close">close</div>', "closeDialog");
+				result += context.actionHtml('<div class="dialog-close button">close</div>', "closeDialog");
 				if (basicTypes.length > 1) {
 					result += '<div class="dialog-title">Select basic type:</div>';
 					for (var i = 0; i < basicTypes.length; i++) {
@@ -132,11 +131,13 @@
 				}
 				result += '</div>';
 			}
-			if (basicTypes.length > 1 && enums == null) {
-				result += context.actionHtml("<span class=\"json-select-type\">T</span>", "openDialog") + " ";
-			}
 			result += context.renderHtml(data, 'data');
-			result += '</span>';
+			if (basicTypes.length > 1 && enums == null) {
+				result = '<span class="dialog-anchor">'
+					+ context.actionHtml("<span class=\"json-select-type button\">T</span>", "openDialog") + " "
+					+ result
+					+ '</span>';
+			}
 			return result;
 		},
 		action: function (context, actionName, basicType) {
@@ -435,12 +436,30 @@
 				}, drawProperty);
 
 				if (canAdd && schemas.allowedAdditionalProperties()) {
-					result += '<tr class="json-object-pair"><td class="json-object-key"><div class="json-object-key-text">';
-					result += context.actionHtml('+ new', "add-new");
-					result += '</div></td><td></td></tr>';
+					if (context.uiState.addInput) {
+						result += '<tr class="json-object-pair"><td class="json-object-key"><div class="json-object-key-text">';
+						result += context.actionHtml('<span class="button">add</span>', "add-confirm");
+						result += '<br>';
+						result += '</div></td><td>';
+						result += '<input type="text" class="json-object-add-input" name="' + context.inputNameForAction("add-input") + '" value="' + Jsonary.escapeHtml(context.uiState.addInputValue) + '"></input>';
+						result += context.actionHtml('<span class="button">cancel</span>', "add-cancel");
+						if (data.property(context.uiState.addInputValue).defined()) {
+							result += '<span class="warning"><code>' + Jsonary.escapeHtml(context.uiState.addInputValue) + '</code> already exists</span>';
+						}
+						result += '</td></tr>';
+					} else {
+						result += '<tr class="json-object-pair"><td class="json-object-key"><div class="json-object-key-text">';
+						result += context.actionHtml('<span class="button">add</span>', "add-input");
+						result += '</div></td><td></td></tr>';
+					}
 				}
 			} else {
-				data.properties(drawProperty);
+				var definedProperties = data.schemas().definedProperties();
+				data.properties(definedProperties, function (key, subData) {
+					if (subData.defined()) {
+						drawProperty(key, subData);
+					}
+				}, true);
 			}
 			result += '</table>';
 			result += '</fieldset>';
@@ -453,9 +472,19 @@
 				data.schemas().createValueForProperty(key, function (newValue) {
 					data.property(key).setValue(newValue);
 				});
-			} else if (actionName == "add-new") {
-				var key = window.prompt("New key:", "key");
+			} else if (actionName == "add-input") {
+				context.uiState.addInput = true;
+				context.uiState.addInputValue = (arg1 == undefined) ? "key" : arg1;
+				return true;
+			} else if (actionName == "add-cancel") {
+				delete context.uiState.addInput;
+				delete context.uiState.addInputValue;
+				return true;
+			} else if (actionName == "add-confirm") {
+				var key = context.uiState.addInputValue;
 				if (key != null && !data.property(key).defined()) {
+					delete context.uiState.addInput;
+					delete context.uiState.addInputValue;
 					data.schemas().createValueForProperty(key, function (newValue) {
 						data.property(key).setValue(newValue);
 					});
@@ -721,7 +750,12 @@
 	Jsonary.render.register({
 		name: "Jsonary plain edit number",
 		renderHtml: function (data, context) {
-			var result = context.actionHtml('<span class="json-number">' + data.value() + '</span>', "input");
+			var style = "";
+			if (data.value().toString().length > 3) {
+				var width = data.value().toString().length;
+				style = 'style="width: ' + width + 'em;"';
+			}
+			var result = '<input class="json-number" type="text" value="' + data.value() + '" name="' + context.inputNameForAction('input') + '" ' + style + '></input>';
 			
 			var interval = data.schemas().numberInterval();
 			if (interval != undefined) {
@@ -729,27 +763,47 @@
 				if (minimum == null || data.value() > minimum + interval || data.value() == (minimum + interval) && !data.schemas().exclusiveMinimum()) {
 					result = context.actionHtml('<span class="json-number-decrement button">-</span>', 'decrement') + result;
 				} else {
-					result = '<span class="json-number-decrement button disabled">-</span>' + result;
+					result = '<span class="json-number-decrement button disabled" onmousedown="event.preventDefault();">-</span>' + result;
 				}
 				
 				var maximum = data.schemas().maximum();
 				if (maximum == null || data.value() < maximum - interval || data.value() == (maximum - interval) && !data.schemas().exclusiveMaximum()) {
 					result += context.actionHtml('<span class="json-number-increment button">+</span>', 'increment');
 				} else {
-					result += '<span class="json-number-increment button disabled">+</span>';
+					result += '<span class="json-number-increment button disabled" onmousedown="event.preventDefault;">+</span>';
 				}
 			}
 			return result;
 		},
-		action: function (context, actionName) {
+		action: function (context, actionName, arg1) {
 			var data = context.data;
 			var interval = data.schemas().numberInterval();
 			if (actionName == "increment") {
-				data.setValue(data.value() + interval);
+				var value = data.value() + interval;
+				var valid = true;
+				var maximum = data.schemas().maximum();
+				if (maximum != undefined) {
+					if (value > maximum || (value == maximum && data.schemas().exclusiveMaximum())) {
+						valid = false;
+					}
+				}
+				if (valid) {
+					data.setValue(value);
+				}
 			} else if (actionName == "decrement") {
-				data.setValue(data.value() - interval);
+				var value = data.value() - interval;
+				var valid = true;
+				var minimum = data.schemas().minimum();
+				if (minimum != undefined) {
+					if (value < minimum || (value == minimum && data.schemas().exclusiveMinimum())) {
+						valid = false;
+					}
+				}
+				if (valid) {
+					data.setValue(value);
+				}
 			} else if (actionName == "input") {
-				var newValueString = prompt("Enter number: ", data.value());
+				var newValueString = arg1
 				var value = parseFloat(newValueString);
 				if (!isNaN(value)) {
 					if (interval != undefined) {
