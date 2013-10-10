f4aba0be45658105e8212f2f499dc1d87b7696a9
Geraint Luff <luffgd@gmail.com>
2013-10-10 18:37:17 +0100

    Update URI Templates

--- jsonary/uri-template.js
+++ jsonary/uri-template.js
@@ -346,7 +346,14 @@ function UriTemplate(template) {
 			var nextPart = textParts[i + 1];
 			var offset = i;
 			while (true) {
-				if (nextPart) {
+				if (offset == textParts.length - 2) {
+					var endPart = substituted.substring(substituted.length - nextPart.length);
+					if (endPart !== nextPart) {
+						return undefined;
+					}
+					var stringValue = substituted.substring(0, substituted.length - nextPart.length);
+					substituted = endPart;
+				} else if (nextPart) {
 					var nextPartPos = substituted.indexOf(nextPart);
 					var stringValue = substituted.substring(0, nextPartPos);
 					substituted = substituted.substring(nextPartPos);
