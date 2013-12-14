2d06e49f383cd05c79e504902bc48af18261f302
Geraint Luff <luffgd@gmail.com>
2013-12-14 09:16:37 +0000

    Remove site

--- express-site/mysql-connect-to-pool.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');
-
-module.exports = mysql.createPool({
-	host: '77.68.58.98',
-	user: 'brokencity1',
-	database: 'brokencity1',
-	password: 'X6VmY3aeefQU'
-});
--- express-site/supervisor-site.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-supervisor -e 'js|json|css' --watch .,../jsonary,../plugins,../renderers,../node-package --ignore public/bundle.js,public/bundle.css,../node-package/core,../node-package/super-bundle site.js
